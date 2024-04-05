3973c739bcfe372eb31f397a4aa0fd1fe460c27c
Esther Adejola <adejolanu@gmail.com>
2024-04-05 15:16:51 +0100

    No changes made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
-
-export default Navbar
+    <nav className="flex items-center justify-between px-32 py-8">
+      <div>
+        <img src="\images\Logo.svg" />
+      </div>
+      <ul className="flex gap-8 border-black border-2 px-8 py-2 rounded-2xl">
+        <li className="text-darkGreen font-semibold">About us</li>
+        <li className="text-darkGreen font-semibold">Our Services</li>
+        <li className="text-darkGreen font-semibold"> Portfolio</li>
+        <li className="text-darkGreen font-semibold">Contact us</li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
