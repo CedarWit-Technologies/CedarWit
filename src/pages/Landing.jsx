a7bf49b386da82a64395fa03da25014dea160520
Esther Adejola <adejolanu@gmail.com>
2024-04-05 16:56:43 +0100

    Added change

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero";
-// import About from "../Components/About";
+import About from "../Components/About";
 // import Services from "../Components/Services";
 // import OurPortfolio from "../Components/OurPortfolio";
 // import ClientReviews from "../Components/ClientReviews";
@@ -9,10 +9,13 @@ import Hero from "../Components/Hero";
 
 const Landing = () => {
   return (
-    <div className="text-darkGreen background-image">
-      <Navbar />
-      <Hero />
-      {/* <About />
+    <div className="text-darkGreen">
+      <main className="background-image pb-16">
+        <Navbar />
+        <Hero />
+      </main>
+      <About />
+      {/* 
       <Services />
       <OurPortfolio />
       <ClientReviews />
