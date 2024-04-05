4e886fa22118cde0fc1faf1f04cd01202c489d1e
Esther Adejola <adejolanu@gmail.com>
2024-04-05 15:31:37 +0100

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Portfolio from "./components/OurPortfolio";
+import Portfolio from "./Components/OurPortfolio";
 
 function App() {
   return (
