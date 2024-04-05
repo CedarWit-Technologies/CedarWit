ffe493bdcfd6fbf279c4961cce08e3c54724db80
Okoye <okoyedivine145@gmail.com>
2024-04-05 18:32:17 +0100

    page is working now

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Portfolio from "./Components/OurPortfolio";
+import OurPortfolio from "./components/OurPortfolio";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/portfolio" element={<OurPortfolio />} />
       </Routes>
     </BrowserRouter>
   );
