dd0253f55a9b678c1fcbc45b8e89e7f5f83106d6
Okoye <okoyedivine145@gmail.com>
2024-04-05 17:56:27 +0100

    minor changes

---
+++ src/components-divine/hooks/useProjects.jsx
@@ -0,0 +1,37 @@
+const useProjects = (n) => {
+  const projects = [
+    {
+      tags: ["Mobile Dev", "Design"],
+      projectName: "Universal Mindfulness",
+      projectDetails:
+        "Revinance is a fintech mobile application used for something and. With features including a wallet system, invest0ment plan and markettplace",
+    },
+    {
+      tags: ["Mobile Dev", "Design"],
+      projectName: "Lunnagram",
+      projectDetails:
+        "Revinance is a fintech mobile application used for something and. With features including a wallet system, invest0ment plan and markettplace",
+    },
+    {
+      tags: ["Mobile Dev", "Design"],
+      projectName: "Royal Convener",
+      projectDetails:
+        "Revinance is a fintech mobile application used for something and. With features including a wallet system, invest0ment plan and markettplace",
+    },
+    {
+      tags: ["Mobile Dev", "Design"],
+      projectName: "Universal Mindfulness",
+      projectDetails:
+        "Revinance is a fintech mobile application used for something and. With features including a wallet system, invest0ment plan and markettplace",
+    },
+    {
+      tags: ["Mobile Dev", "Design"],
+      projectName: "Universal Mindfulness",
+      projectDetails:
+        "Revinance is a fintech mobile application used for something and. With features including a wallet system, invest0ment plan and markettplace",
+    },
+  ];
+  return projects;
+};
+
+export default useProjects;
