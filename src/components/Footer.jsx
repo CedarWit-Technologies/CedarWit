c734f4a582c96a1e51c65cdd72d7b8365116a5f2
Esther Adejola <adejolanu@gmail.com>
2024-05-05 19:27:00 +0100

    updated content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,22 +6,24 @@ import { FaFacebook } from "react-icons/fa6";
 const Footer = () => {
   return (
     <footer className="bg-lightGreen text-white lg:px-32 lg:py-8 text-lg font-semibold p-6">
-      <div className="grid grid-cols-4 md:gap-8 lg:gap-0">
+      <div className="grid grid-cols-5 md:gap-8 lg:gap-6">
         <div className="sm:col-span-3 sm:mb-16 md:col-span-4 md:mb-12 lg:col-span-1">
-          <div className="mb-2 lg:mr-16">
+          <div className="mb-2 ">
             {" "}
             <img className="sm:w-36 sm:mb-4" src="images/FooterLogo.svg" />
           </div>
-          <p className="text-base sm:text-sm">Bringing your ideas to reality</p>
+          <p className="text-base sm:text-sm ">
+            Bringing your ideas to reality
+          </p>
         </div>
-        <div className="sm:col-span-2">
+        <div className="sm:col-span-2 lg:col-span-1 lg:ml-8">
           <h1 className="text-2xl text-darkerGreen font-bold mb-6">Company</h1>
           <p className="mb-6 text-base">About Us</p>
           <p className="mb-6 text-base">OurServices</p>
           <p className="mb-6 text-base">Portfolio</p>
           <p className="mb-6 text-base">Contact Us</p>
         </div>
-        <div className="sm:col-span-2">
+        <div className="sm:col-span-2 lg:ml-2">
           <h1 className="text-2xl text-darkerGreen font-bold mb-6 ">
             Services
           </h1>
@@ -30,11 +32,11 @@ const Footer = () => {
           <p className="mb-6 text-base">Production</p>
           <p className="mb-6 text-base">digital marketing</p>
         </div>
-        <div className="sm:col-span-4 md:col-span-2 lg:col-span-1">
+        <div className="sm:col-span-4 md:col-span-2 lg:col-span-2 lg:ml-10">
           <h1 className="text-2xl mb-6 text-darkerGreen font-bold">
             Contact Information
           </h1>
-          <div className="flex gap-2 items-center mb-6">
+          <div className="flex gap-2 items-center mb-6 ">
             <div>
               <IoIosMail />
             </div>
@@ -53,12 +55,12 @@ const Footer = () => {
           <div className="flex gap-4 items-center">
             <div>
               <a href="https://www.facebook.com/share/w7YzCy636McnEEfa/?mibextid=qi2Omg">
-                <FaFacebook size={"1.5em"} />
+                <FaFacebook size={"1.25em"} />
               </a>
             </div>
             <div>
               <a href="https://x.com/CedarWitTech?t=AjmovdfUYlq6fNjkOPUERA&s=09">
-                <FaXTwitter size={"1.5em"} />
+                <FaXTwitter size={"1.25em"} />
               </a>
             </div>
             <div>
