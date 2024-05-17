9bdb1c376a3f7f02838cd078562d6694128af544
Okoye <okoyedivine145@gmail.com>
2024-05-17 21:40:53 +0100

    Animated loading states.

--- src/components/GraphicsProjects.jsx
+++ src/components/GraphicsProjects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import gp1 from "../assets/graphics-projects/gp1.png";
 import gp2 from "../assets/graphics-projects/gp2.png";
 import gp3 from "../assets/graphics-projects/gp3.png";
@@ -14,10 +14,6 @@ import { useContext, useState } from "react";
 const GraphicsProjects = () => {
   const [isLoading, setIsLoading] = useState(true);
 
-  const handleLoad = () => {
-    setIsLoading(false);
-  };
-
   const projects = [gp1, gp2, gp3, gp4, gp5, gp6, gp7, gp8, gp9];
   const col1 = projects.slice(0, 3);
   const col2 = projects.slice(3, 6);
@@ -25,43 +21,54 @@ const GraphicsProjects = () => {
   const columns = [col1, col2, col3];
 
   const { page, setOnPage } = useContext(PageContext);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
-      <div className="hidden lg:flex gap-6 mt-10">
-        {columns.map((col, id) => (
-          <div key={id} className="flex flex-col gap-6">
-            {col.map((col, index) => (
-              <>
-                {isLoading && (
-                  <div className="w-60 h-40 animate-pulse-quick bg-gray-200"></div>
-                )}
-                <div
-                  key={index}
-                  className={`${
-                    isLoading ? "hidden" : "block"
-                  } hover:shadow-custom3 hover:-translate-y-[3px] transition-all duration-300 ease-in-out ${
-                    index == 2 && "lg:hidden"
-                  } ${id == 1 && index == 1 && "lg:hidden"}`}
-                >
-                  {page}
-                  <img onLoad={handleLoad} src={col} />
-                </div>
-              </>
+      {isLoading && (
+        <div className="w-[30rem] place-content-center h-[20rem] bg-gray-300 animate-pulse rounded-lg"></div>
+      )}
+      {!isLoading && (
+        <>
+          <div className="hidden lg:flex gap-6 mt-10">
+            {columns.map((col, id) => (
+              <div key={id} className="flex flex-col gap-6">
+                {col.map((col, index) => (
+                  <>
+                    <div
+                      key={index}
+                      className={`${
+                        isLoading ? "hidden" : "block"
+                      } hover:shadow-custom3 hover:-translate-y-[3px] transition-all duration-300 ease-in-out ${
+                        index == 2 && "lg:hidden"
+                      } ${id == 1 && index == 1 && "lg:hidden"}`}
+                    >
+                      {page}
+                      <img onLoad={handleLoad} src={col} />
+                    </div>
+                  </>
+                ))}
+              </div>
             ))}
           </div>
-        ))}
-      </div>
 
-      <div className="flex lg:hidden flex-col gap-6">
-        {col1.map((col, id) => (
-          <div
-            key={id}
-            className="hover:shadow-custom3 hover:-translate-y-[3px] transition-all duration-300 ease-in-out"
-          >
-            <img className="max-w-[500px]" src={col} />
+          <div className="flex lg:hidden flex-col gap-6">
+            {col1.map((col, id) => (
+              <div
+                key={id}
+                className="hover:shadow-custom3 hover:-translate-y-[3px] transition-all duration-300 ease-in-out"
+              >
+                <img className="max-w-[500px]" src={col} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </>
   );
 };
