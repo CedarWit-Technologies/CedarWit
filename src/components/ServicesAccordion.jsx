ba13f8080cd79e5528397063a97d86be1a6f62b5
Esther Adejola <adejolanu@gmail.com>
2024-05-29 13:05:28 +0100

    Added changes

--- src/components/ServicesAccordion.jsx
+++ src/components/ServicesAccordion.jsx
@@ -16,7 +16,7 @@ const ServicesAccordion = () => {
           onClick={() => handleClick(index)}
           className={`border-white border-t border-b text-2xl w-full flex flex-col justify-center overflow-hidden transition-all duration-300 ease-in-out relative ipad:text-2xl ${
             opened == index
-              ? "h-40 justify-start border-l-4 sm:h-48 ipad:h-64"
+              ? "h-40 justify-start border-l-4 sm:h-52 ipad:h-64"
               : "h-16 border-l-0"
           }`}
         >
@@ -41,7 +41,7 @@ const ServicesAccordion = () => {
           </span>
 
           <p
-            className={`text-xs sm:text-[10px] ipad:text-sm md:text-base tab:text-sm font-normal lg:text-base px-3 transition-all duration-300 ease-in-out ${
+            className={`text-xs sm:text-[14px] ipad:text-sm md:text-base tab:text-sm font-normal lg:text-base px-3 transition-all duration-300 ease-in-out  ${
               opened == index
                 ? "mt-2 sm:h-24 lg:h-32 ipad:h-32 opacity-100 ipad:ml-3 md:-translate-y-4"
                 : "h-0 opacity-0"
